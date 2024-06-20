56bef9bdbbf96680ee432bdb106a48d7130a8ca6
Ido V. Z <programerido@gmail.com>
2024-06-20 21:23:49 +0300

    Create ravendb backend

---
+++ user-search-backend-ravendb/src/javascript/client.js
@@ -0,0 +1,9 @@
+// import { DocumentStore } from "ravendb"
+// import { readFileSync } from "fs"
+// const authOptions = {
+//     certificate: readFileSync(process.env.CERTIFICATE_PATH as string)
+// }
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.TEST);
+console.log("test");
---
+++ user-search-backend-ravendb/src/typescript/client.ts
@@ -0,0 +1,7 @@
+// import { DocumentStore } from "ravendb"
+// import { readFileSync } from "fs"
+
+// const authOptions = {
+//     certificate: readFileSync(process.env.CERTIFICATE_PATH as string)
+// }
+
