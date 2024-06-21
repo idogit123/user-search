7aee09adbd30e10addcdd5c9967c38cf4edf6070
Ido V. Z <programerido@gmail.com>
2024-06-21 09:32:41 +0300

    Fix table

--- user-search-frontend/src/components/SearchResults.tsx
+++ user-search-frontend/src/components/SearchResults.tsx
@@ -3,15 +3,17 @@ import styles from '@/styles/searchResults.module.css'
 export default function SearchResults()
 {
     return <table id={styles.table}>
-        <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>City</th>
-        </tr>
-        <tr>
-            <td>Ido</td>
-            <td>Vitman Zilber</td>
-            <td>Binyamina</td>
-        </tr>
+        <tbody>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>City</th>
+            </tr>
+            <tr>
+                <td>Ido</td>
+                <td>Vitman Zilber</td>
+                <td>Binyamina</td>
+            </tr>
+        </tbody>
     </table>
 }
