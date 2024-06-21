6cb834a055b881a02376da142b1fb67f726725ef
Ido V. Z <programerido@gmail.com>
2024-06-21 23:14:36 +0300

    Create User.ts

---
+++ user-search-frontend/src/types/User.ts
@@ -0,0 +1,12 @@
+export class User {
+    firstName: string
+    lastName: string
+    city: string
+
+    constructor(firstName: string, lastName: string, city: string)
+    {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.city = city
+    }
+}
