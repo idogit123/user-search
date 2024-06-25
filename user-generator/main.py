e9fa7c8e3e1be903c6141924de5b2063a054107a
Ido V. Z <programerido@gmail.com>
2024-06-25 12:04:41 +0300

    Convert user generator to csv
    Because RavenDB cant import json data

--- user-generator/main.py
+++ user-generator/main.py
@@ -1,14 +1,29 @@
 from faker import Faker
+from csv import writer as csv_writer
 
 faker = Faker()
 
 def fake_user():
-    return {
-        'firstName': faker.first_name(),
-        'lastName': faker.last_name(),
-        'city': faker.city()
-    }
+    return [
+        faker.first_name(), # first name
+        faker.last_name(), # last name
+        faker.city() # city name
+    ]
 
-print(
-    fake_user()
-)
+def fake_users_to_csv(num_of_users):
+    users = []
+    for _  in range(num_of_users):
+        users.append( fake_user() )
+
+    with open('user-generator/fake_users.csv', 'w') as csv_file:
+        writer = csv_writer(csv_file, lineterminator='\n')
+        
+        # write headers.
+        writer.writerow(['firstName', 'lastName', 'city'])
+        # write users
+        writer.writerows(users)
+
+def main():
+    fake_users_to_csv(10)
+
+main()
