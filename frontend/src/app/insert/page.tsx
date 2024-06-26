e78c55fbb530b26772ac4bd9196766ebee0f0e73
Ido V. Z <programerido@gmail.com>
2024-06-26 18:00:30 +0300

    Create insert page

---
+++ frontend/src/app/insert/page.tsx
@@ -0,0 +1,11 @@
+"use server";
+
+export default async function BulkInsertPage()
+{
+    return <main>
+        <form action="">
+            <input type="file" name="path" id="" />
+            <button>Import</button>
+        </form>
+    </main>
+}
