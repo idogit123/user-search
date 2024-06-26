a9a1cc533ffc27c82095b1be7e37241c9bc2fe0d
Ido V. Z <programerido@gmail.com>
2024-06-26 17:26:41 +0300

    Fix user generator

--- user-generator/index.js
+++ user-generator/index.js
@@ -3,23 +3,30 @@ const fs = require("fs");
 
 function createRandomUser() {
     return {
-        userId: faker.string.uuid(),
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        avatar: faker.image.avatar(),
-        birthdate: faker.date.birthdate(),
-        registeredAt: faker.date.past(),
-        address: {
-            city: faker.location.city()
-        },
         firstName: faker.person.firstName(),
-        lastName: faker.person.lastName()
+        lastName: faker.person.lastName(),
+        address: {
+            country: faker.location.country(),
+            city: faker.location.city(),
+            streetAddress: faker.location.streetAddress(),
+            zipCode: faker.location.zipCode()
+        },
+        contact: {
+            phone: faker.phone.number(),
+            email: faker.internet.email(),
+            instegram: faker.internet.userName()
+        },
+        job: {
+            company: faker.company.name(),
+            title: faker.person.jobTitle(),
+            type: faker.person.jobType()
+        }
     };
 }
 
 const writeStream = fs.createWriteStream('users.jsonl', { flags: 'w' });
 
-for (let i = 0; i < 10_000_000; i++) {
+for (let i = 0; i < 1_000_000; i++) {
     const user = createRandomUser();
     writeStream.write(JSON.stringify(user)); 
     writeStream.write('\n')
@@ -30,8 +37,10 @@ for (let i = 0; i < 10_000_000; i++) {
 }
 
 // Close the stream when done
-writeStream.end();
-
-writeStream.on('finish', () => {
+writeStream.end(() => {
     console.log('Users written to users.jsonl');
 });
+
+writeStream.on('error', (error) => {
+    console.log(error)
+})
