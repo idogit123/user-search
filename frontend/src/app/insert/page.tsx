874d37a6ac71aa912c0950d5e174f74247043ab2
Ido V. Z <programerido@gmail.com>
2024-06-27 11:31:45 +0300

    edit bulk insert page

--- frontend/src/app/insert/page.tsx
+++ frontend/src/app/insert/page.tsx
@@ -1,11 +1,29 @@
 "use server";
 
-export default async function BulkInsertPage()
-{
-    return <main>
-        <form action="">
-            <input type="file" name="path" id="" />
-            <button>Import</button>
+import FormButton from '@/components/FormButton';
+import styles from './page.module.css'
+import { redirect } from 'next/navigation';
+
+async function insertUsersToDatabase() {
+    "use server";
+
+    const response = await fetch('http://localhost:8080/insert')
+    const durationInMs = (await response.json()).durationInMs
+
+    redirect(`http://localhost:3000/insert?duration=${durationInMs}`)
+}
+
+export default async function BulkInsertPage(
+    { searchParams: { duration } }: { searchParams: { duration: number | undefined } }
+) {
+    return <main id={styles.page}>
+        <form id={styles.form} action={insertUsersToDatabase}>
+            <div id={styles.titleContainer}>
+                <h2 id={styles.title}>Bulk Insert</h2>
+                <p id={styles.secondTitle}>Import users from json file to database.</p>
+                <p id={styles.timer}>Completed in: <span>{duration}</span>ms</p>
+            </div>
+            <FormButton label="Insert"/>
         </form>
     </main>
 }
