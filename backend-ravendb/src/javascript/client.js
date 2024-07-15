bc1e4ac44009bdde3347dd1c8a7f159edc771598
Ido V. Z <programerido@gmail.com>
2024-07-15 16:24:02 +0300

    Install final fix for bulkInsert

--- backend-ravendb/src/javascript/client.js
+++ backend-ravendb/src/javascript/client.js
@@ -54,7 +54,7 @@ export async function bulkInsertUsers() {
         });
         for await (const line of readline) {
             const user = new User(JSON.parse(line));
-            // await bulkInsert.store(user)
+            await bulkInsert.store(user);
         }
         readline.close();
         await bulkInsert.finish();
