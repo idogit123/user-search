84c4c02e95a3369684ff8f5d1dc879420e8e03fd
Ido V. Z <programerido@gmail.com>
2024-08-11 12:29:32 +0300

    Fix mongodb insert

--- backend-mongodb/src/javascript/client.js
+++ backend-mongodb/src/javascript/client.js
@@ -1,7 +1,4 @@
 import { MongoClient } from "mongodb";
-import { User } from "./User.js";
-import { createInterface } from "readline";
-import { createReadStream, readdirSync } from "fs";
 import { Timer } from "./Timer.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -35,33 +32,4 @@ export async function getUsers(query, sort, isDescending, page) {
         users: users,
         durationInMs: timer.getDuration()
     };
-}
-export async function bulkInsert() {
-    const userFiles = readdirSync(process.env.USERS_DIR);
-    timer.start();
-    for await (const userFilePath of userFiles) {
-        const readline = createInterface({
-            input: createReadStream(`${process.env.USERS_DIR}/${userFilePath}`),
-            crlfDelay: Infinity
-        });
-        const MAX_BATCH_SIZE = 1000;
-        let batch = [];
-        for await (const line of readline) {
-            const user = new User(JSON.parse(line));
-            batch.push(user);
-            if (batch.length >= MAX_BATCH_SIZE) {
-                await usersCollection.insertMany(batch);
-                console.log(`inserted batch, size: ${batch.length}`);
-                batch = [];
-            }
-        }
-        // insert remaining users
-        if (batch.length > 0) {
-            await usersCollection.insertMany(batch);
-            console.log(`insert remaining users, size: ${batch.length}`);
-        }
-        readline.close();
-    }
-    timer.end();
-    return timer.getDuration();
 }
---
+++ backend-mongodb/src/javascript/insert.js
@@ -0,0 +1,42 @@
+import { MongoClient } from "mongodb";
+import { User } from "./User.js";
+import { createInterface } from "readline";
+import { createReadStream, readdirSync } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+const databaseInfo = {
+    serverPath: process.env.SERVER_PATH,
+    databaseName: process.env.DATABASE_NAME,
+    collectionName: process.env.COLLECTION_NAME
+};
+const client = new MongoClient(databaseInfo.serverPath);
+const usersDB = client.db(databaseInfo.databaseName);
+const usersCollection = usersDB.collection(databaseInfo.collectionName);
+export async function bulkInsertUsers() {
+    const userFiles = readdirSync(process.env.USERS_DIR);
+    for await (const userFilePath of userFiles) {
+        const readline = createInterface({
+            input: createReadStream(`${process.env.USERS_DIR}/${userFilePath}`),
+            crlfDelay: Infinity
+        });
+        const MAX_BATCH_SIZE = 1000;
+        let batch = [];
+        for await (const line of readline) {
+            const user = new User(JSON.parse(line));
+            batch.push(user);
+            if (batch.length >= MAX_BATCH_SIZE) {
+                await usersCollection.insertMany(batch);
+                batch = [];
+            }
+        }
+        // insert remaining users
+        if (batch.length > 0) {
+            await usersCollection.insertMany(batch);
+            console.log(`insert remaining users, size: ${batch.length}`);
+        }
+        console.log('insered file: ', userFilePath);
+        readline.close();
+    }
+}
+await bulkInsertUsers();
+client.close();
