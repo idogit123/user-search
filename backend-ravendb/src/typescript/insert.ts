f15ffbbdd0229b266015e79615e29ea7ecfa9663
Ido V. Z <programerido@gmail.com>
2024-08-11 13:45:35 +0300

    Add timer

--- backend-ravendb/src/typescript/insert.ts
+++ backend-ravendb/src/typescript/insert.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import { User } from "./User.js"
 import { createInterface } from "readline"
 import { createReadStream, readdirSync } from 'fs'
+import { Timer } from "./Timer.js"
 dotenv.config()
 
 const authOptions: IAuthOptions = {
@@ -43,6 +44,11 @@ export async function bulkInsertUsers()
     }
 }
 
+const timer = new Timer()
+
+timer.start()
 await bulkInsertUsers()
+timer.end()
+console.log("Bulk Insert Took: " + timer.getDuration())
 
 documentStore.dispose()
