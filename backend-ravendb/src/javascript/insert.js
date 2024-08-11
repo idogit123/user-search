8d5b9b5824ba9ed8dbefafabbfea882934f3483b
Ido V. Z <programerido@gmail.com>
2024-08-11 13:49:50 +0300

    Add timer

--- backend-ravendb/src/javascript/insert.js
+++ backend-ravendb/src/javascript/insert.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import { User } from "./User.js";
 import { createInterface } from "readline";
 import { createReadStream, readdirSync } from 'fs';
+import { Timer } from "./Timer.js";
 dotenv.config();
 const authOptions = {
     certificate: readFileSync(process.env.CERTIFICATE_PATH),
@@ -29,5 +30,9 @@ export async function bulkInsertUsers() {
         console.log('FINISHED Inserting file: ', userFilePath);
     }
 }
+const timer = new Timer();
+timer.start();
 await bulkInsertUsers();
+timer.end();
+console.log("Bulk Insert Took: " + timer.getDuration());
 documentStore.dispose();
