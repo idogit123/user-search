85b95977de15487c269efb4ba7d71e1ac59c4e1b
Ido V. Z <programerido@gmail.com>
2024-08-25 10:49:12 +0300

    Create insert.ts

---
+++ backend-ravendb/src/typescript/insert.ts
@@ -0,0 +1,44 @@
+import { DocumentStore } from "ravendb"
+import dotenv from "dotenv"
+import { User } from "./User.js"
+import { createInterface } from "readline"
+import { createReadStream } from 'fs'
+import { Timer } from "./Timer.js"
+dotenv.config()
+
+const documentStore = new DocumentStore(
+    process.env.SERVER_ADDRESS as string,
+    process.env.DATABASE_NAME as string, 
+)
+documentStore.initialize()
+
+async function bulkInsertUsers()
+{
+    const readline = createInterface({
+        input: createReadStream(`${process.env.USERS_DIR}/users.jsonl`),
+        crlfDelay: Infinity
+    })
+
+    let counter = 0
+    for await (const line of readline)
+    {
+        const user = new User(JSON.parse(line))
+
+        const session = documentStore.openSession()
+        await session.store<User>(user, user.getId(counter))
+        await session.saveChanges()
+
+        counter++
+    }
+
+    readline.close()
+}
+
+const timer = new Timer()
+
+timer.start()
+await bulkInsertUsers()
+timer.end()
+console.log("duration: " + timer.getDuration())
+
+documentStore.dispose() 
